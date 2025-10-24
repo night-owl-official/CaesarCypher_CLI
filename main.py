8db76c1f0d98c4093dd7262483c0d49901a7a5bd
Daniele Grieco <daniele.grieco@cyber-core.it>
2025-10-24 10:40:07 +0200

    Implement program loop in the main file, only stopping when the user decides

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ from caesar import caesar
 if __name__ == "__main__":
 
     print(art.logo)
-    dir = input("Type 'encrypt (e)' to encrypt, type 'decrypt (d)' to decrypt:\n")
-    txt = input("Type your message:\n")
-    shft = int(input("Type the shift number:\n"))
 
-    caesar(dir, txt, shft)
+    is_running = True
+
+    while is_running:
+        dir = input("Type 'encrypt (e)' to encrypt, type 'decrypt (d)' to decrypt:\n")
+        txt = input("Type your message:\n")
+        shft = int(input("Type the shift number:\n"))
+
+        caesar(dir, txt, shft)
+
+        print("\nDo you want to go again?")
+        user_choice = input("Type 'no (n)' to stop, any other input will be interpreted as 'yes'.\n").lower()
+        if user_choice == 'no' or user_choice == 'n':
+            is_running = False
+            print("\nGoodbye!")
+
+        print() # Print a newline for better readability between runs
